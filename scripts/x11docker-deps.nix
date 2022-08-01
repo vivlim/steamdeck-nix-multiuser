eacb6f3ad0fbcc484ecb6728a8a5815da864df70
Viv Lim <vivlim@vivl.im>
2022-07-31 17:22:08 -0700

    initial script to run nested x session in container with gpu accel.
    audio not working, dockerfile from stdin not tested.

---
+++ scripts/x11docker-deps.nix
@@ -0,0 +1,18 @@
+let
+    pkgs = import <nixpkgs> {};
+    nixGLPkgs = import (pkgs.fetchFromGitHub { owner="guibou"; repo="nixGL"; rev="047a34b2f087e2e3f93d43df8e67ada40bf70e5c"; sha256="4b3d2e5aca6abec84615b4fe5e645555acae5b9d78acd34b2efaeb7bc8c12cb5";}) {};
+in
+pkgs.stdenv.mkDerivation {
+  name = "x11docker-steamdeck-nix";
+
+  buildInputs =
+  [
+    pkgs.x11docker
+    pkgs.tmux
+    pkgs.docker
+    pkgs.tini
+    pkgs.weston
+    pkgs.xwayland
+    nixGLPkgs.auto.nixGLDefault
+  ];
+}
